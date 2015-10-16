32f0f2b6b97da53443086cefd6c62ecc83e49a13
Jamie David <jjdavid14@live.com>
2015-10-15 23:19:25 -0700

    add loop and removeSymbols() method

--- Palindrome.java
+++ Palindrome.java
@@ -5,37 +5,52 @@ public class Palindrome {
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		String phrase = in.nextLine();
-		phrase = phrase.toUpperCase();
 		
-		int sizeOfPhrase = phrase.length();
-		
-		myQueue queue = new myQueue(sizeOfPhrase);
-		Stack<Character> stack = new Stack<Character>();
-		
-		for(int i=0;i<sizeOfPhrase;i++){
-			char charToPush = phrase.charAt(i);
-			if(charToPush != ' '){
-				stack.push(charToPush);
-				queue.enqueue(charToPush);
+		while(in.hasNextLine()){
+			String phrase = in.nextLine();
+			phrase = phrase.toUpperCase();
+			
+			int sizeOfPhrase = phrase.length();
+			
+			myQueue queue = new myQueue(sizeOfPhrase);
+			Stack<Character> stack = new Stack<Character>();
+			
+			phrase = removeSymbols(phrase);
+			
+			for(int i=0;i<sizeOfPhrase;i++){
+				char charToPush = phrase.charAt(i);
+				if(charToPush != ' '){
+					stack.push(charToPush);
+					queue.enqueue(charToPush);
+				}
+			}
+			
+			boolean isPalindrome = false;
+			
+			for(int i=0;!stack.isEmpty() && !queue.isEmpty();i++){
+				if(stack.pop() != queue.dequeue()){
+					isPalindrome = false;
+					break;
+				}else{
+					isPalindrome = true;
+				}
+			}
+			
+			if(isPalindrome){
+				System.out.println("Word is a PALINDROME.");
+			} else {
+				System.out.println("Word is NOT a PALINDROME.");
 			}
 		}
-		
-		boolean isPalindrome = false;
-		
-		for(int i=0;!stack.isEmpty() && !queue.isEmpty();i++){
-			if(stack.pop() != queue.dequeue()){
-				isPalindrome = false;
-				break;
-			}else{
-				isPalindrome = true;
+	}
+	
+	public static String removeSymbols(String line){
+		char[] array = line.toCharArray();
+		for(int i=0;i<array.length;i++){
+			if(Character.isAlphabetic(array[i]) == false){
+				array[i] = ' ';
 			}
 		}
-		
-		if(isPalindrome){
-			System.out.println("Word is a PALINDROME.");
-		} else {
-			System.out.println("Word is NOT a PALINDROME.");
-		}
+		return new String(array);
 	}
 }
